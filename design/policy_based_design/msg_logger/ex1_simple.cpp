f7b651281a77081db4a3598833ad7977f09ced9e
saru@southpole <saurabhsawant@lbl.gov>
2024-01-31 11:42:22 -0800

    modified msg_logger/ex1

--- design/policy_based_design/msg_logger/ex1_simple.cpp
+++ design/policy_based_design/msg_logger/ex1_simple.cpp
@@ -2,90 +2,168 @@
  *
  * see: https://stackoverflow.com/questions/20276659/policy-based-approach-with-logger
  */
-#include <cstdlib>
-#include <utility>
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <iomanip>
+#include <filesystem>
+#include <mutex>
 
 struct WriteToConsole 
 {
-    void operator() (const std::string& msg) const {
+    void operator() (const std::string& msg) {
         std::cout << msg << "\n";
     }
 };
 
+struct WriteToFile 
+{
+    explicit WriteToFile(std::string filename) 
+    {
+        try 
+        {
+            /* lock the mutex for thread-safety.
+             * if filenames are guaranteed to be unique to each thread, 
+             * then _mutex is not necessary.
+             */
+            std::lock_guard<std::mutex> lock(_mutex);
 
-template <typename StreamPolicy=WriteToConsole>
+            if (std::filesystem::exists(filename)) {
+                std::ofstream clearFile(filename.c_str(), std::ios::trunc);        
+                if (!clearFile.is_open()) {
+                    throw std::runtime_error("Error clearing file: " + filename);
+                }
+            }
+
+            _file.open(filename.c_str(), std::ios::app);
+
+            if(!_file.is_open()) {
+                throw std::runtime_error("Error opening file: " + filename);
+            }
+        }
+        catch (...) {
+            // rethrow exception after unlocking mutex
+            _mutex.unlock();
+            throw; 
+        }
+    }
+    void operator() (const std::string& msg) 
+    {
+        try {
+            std::lock_guard<std::mutex> lock(_mutex);
+
+            _file << msg << "\n";
+        }
+        catch (...) {
+            _mutex.unlock();
+            throw; 
+        }
+    }
+
+    WriteToFile(const WriteToFile&) = delete;
+    WriteToFile& operator=(const WriteToFile&) = delete;
+
+    WriteToFile(WriteToFile&& that) : 
+        _file(std::move(that._file))
+    {}
+
+    WriteToFile& operator=(WriteToFile&& that) noexcept 
+    {
+        if(this == &that) return *this;
+
+        _file = std::move(that._file);
+        return *this;
+    }
+
+    ~WriteToFile() noexcept 
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        if(_file.is_open()) _file.close();
+    }  
+
+private:
+    std::ofstream _file;
+    std::mutex _mutex;
+};
+
+
+struct WithTimeStamp
+{
+    static std::string get_timestamp()
+    {
+        auto timestamp = std::chrono::system_clock::now();
+        std::time_t now_c = std::chrono::system_clock::to_time_t(timestamp);
+
+        std::ostringstream oss;
+        oss << std::put_time(std::localtime(&now_c), "%T");
+        return "[" + oss.str() + "]: ";
+    }
+};
+
+struct NoTimeStamp
+{
+    static std::string get_timestamp()
+    {
+        return std::string();
+    }
+};
+
+template <typename StreamPolicy=WriteToConsole, typename TimePolicy=NoTimeStamp>
 class MsgLogger : private StreamPolicy
 {
-    std::string _msg;
 public:
-    /*pass-by-value and then move: 
-     * best case: copy elision (compiler will elid the copy) and members are move constructed
-     * worst case: when passing l-value, copy and then move is performed
+    /* Preference for r-value references.
+     * l-value strings must be moved while passing as arguments.
      */
-    //explicit MsgLogger(std::string&& msg = "*****",
-    //                   StreamPolicy stream_policy = StreamPolicy()) : 
-    //_msg(std::move(msg)),
-    //StreamPolicy(std::move(stream_policy))
-    //{
-    //    StreamPolicy::operator()(_msg);
-    //}
-
-    /*pass-by-r-value reference: 
-     * l-values cannot be passed, but to be moved while passing as arguments
-     */
-    explicit MsgLogger(std::string&& msg = "*****",
-                       StreamPolicy&& stream_policy = StreamPolicy()) : 
-    _msg(std::move(msg)),
+    explicit MsgLogger(StreamPolicy&& stream_policy = StreamPolicy(),
+                       std::string&& init_msg = "###\n") : 
     StreamPolicy(std::move(stream_policy))
     {
-        StreamPolicy::operator()(_msg);
+        StreamPolicy::operator()("\n" + TimePolicy::get_timestamp() + init_msg);
     }
 
-    /*copy constructor*/
-    MsgLogger(const MsgLogger& that) :
-        _msg(that._msg),
-        StreamPolicy(that)
+    void operator() (std::string msg) 
     {
-        StreamPolicy::operator()(_msg);
+        StreamPolicy::operator()(TimePolicy::get_timestamp() + msg);
     }
 
-    /*copy assignment operator*/
-    MsgLogger& operator=(const MsgLogger& that) 
-    {
-        if(this == &that) return *this;
-        _msg = that._msg;
-        StreamPolicy::operator=(that);
-        StreamPolicy::operator()(_msg);
-        return *this;
-    }
+   ~MsgLogger() = default;
+    MsgLogger(const MsgLogger& that) = delete;
+    MsgLogger& operator=(const MsgLogger& that) = delete;
 
-    /*move constructor*/
     MsgLogger(MsgLogger&& that) : 
-        _msg(std::move(that._msg)), 
         StreamPolicy(std::move(that))
-    {
-        StreamPolicy::operator()(_msg);
-    }
+    {}
 
-    /*move assignment operator*/
-    MsgLogger& operator=(MsgLogger&& that) 
+    MsgLogger& operator=(MsgLogger&& that) noexcept
     {
         if(this == &that) return *this;
-        _msg = std::move(that._msg);
+
         StreamPolicy::operator=(std::move(that));
-        StreamPolicy::operator()(_msg);
         return *this;
     }
+
 };
 
 int main() 
 {
-    MsgLogger clog{}; /*default constructor*/
-    MsgLogger clog2{"Hello from clog2"};
-    MsgLogger clog3{clog2}; /*copy constructor*/
-    clog = clog2; /*copy assignment operator*/
-    MsgLogger clog4{std::move(clog2)}; /*move constructor*/
-    clog = std::move(clog4); /*move assignment operator*/
+    MsgLogger clogger{WriteToConsole(), "Hello, this is clogger!"};
+    clogger("clogger is logging...");
+
+    MsgLogger<WriteToConsole, WithTimeStamp> ctlogger{WriteToConsole(), "Hello from ctlogger!"};
+    ctlogger("ctlogger is logging...");
+
+    MsgLogger<WriteToFile, WithTimeStamp> ftlogger{WriteToFile("file1.txt"), "Hello from ftlogger!"};
+    ftlogger("ftlogger is logging...");
+
+    std::string header = "Hello from ftlogger2!";
+    MsgLogger<WriteToFile, WithTimeStamp> ftlogger2{WriteToFile("file1.txt"), std::move(header)};
+    ftlogger2("ftlogger2 is logging...");
+    ftlogger("ftlogger is still logging...");
+    ftlogger2("ftlogger2 is also still logging...");
+
+    MsgLogger<WriteToFile, WithTimeStamp> ftlogger3{std::move(ftlogger2)};
+    ftlogger3("ftlogger3 started logging...");
+
 }
