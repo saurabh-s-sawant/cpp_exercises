5fa9e1f32f7102df047c883d7982ce4393f8b570
saru@southpole <saurabhsawant@lbl.gov>
2024-04-23 10:08:14 -0700

    removed graphene_impl

--- design/type_erasure/algorithm_impl/graphene_impl.cpp
+++
@@ -1,6 +0,0 @@
-#include "materials_impl.H"
-#include <iostream>
-
-void computeStep1(Graphene const& graphene) {
-    std::cout << "processing step 1 for Graphene with unitcells: " << graphene.get_unitcells() << "\n";
-}
