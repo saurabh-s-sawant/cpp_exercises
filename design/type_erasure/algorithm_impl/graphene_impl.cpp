99052d231d4c445ea32ea6d31bdc3f9b92b358d1
saru@southpole <saurabhsawant@lbl.gov>
2024-04-23 10:09:01 -0700

    added graphene_impl.cpp

---
+++ design/type_erasure/algorithm_impl/graphene_impl.cpp
@@ -0,0 +1,6 @@
+#include "materials_impl.H"
+#include <iostream>
+
+void computeStep1(Graphene const& graphene) {
+    std::cout << "processing step 1 for Graphene with unitcells: " << graphene.get_unitcells() << "\n";
+}
